d95d6e346fa180c25a449b2f392ea254a8ff738b
Katie Bridson <kbridson@memphis.edu>
2024-09-09 13:35:19 -0500

    Added shell script and python argparse script for greetings

---
+++ greeting.py
@@ -0,0 +1,19 @@
+#!/Users/kbrid/.asdf/shims/python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("time")
+parser.add_argument("name")
+parser.add_argument("-e", "--excited", action="store_true")
+
+args = parser.parse_args()
+
+time = args.time
+name = args.name
+excited = args.excited
+punct = "!"
+if excited:
+    punct = "!!!"
+
+print(f"Good {time}, {name}{punct}")
---
+++ greeting.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+TIME=$1
+NAME=$2
+MSG=$3
+
+echo "Good $TIME, $NAME! $MSG!"
